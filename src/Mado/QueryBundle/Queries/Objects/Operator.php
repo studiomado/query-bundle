79a90cff5cf98de46a791aad7c400afc6362a48e
Simone Gentili <sensorario@gmail.com>
2018-01-03 05:47:34 +0100

    update meta files

--- src/Mado/QueryBundle/Queries/Objects/Operator.php
+++ src/Mado/QueryBundle/Queries/Objects/Operator.php
@@ -2,6 +2,7 @@
 
 namespace Mado\QueryBundle\Queries\Objects;
 
+/** @deprecated since version 2.3 will be removed in version 2.4 */
 final class Operator
 {
     public static function fromString(string $operatorName)
