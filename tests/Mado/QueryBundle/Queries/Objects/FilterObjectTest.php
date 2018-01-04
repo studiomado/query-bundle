2294807d2cda841ddff9ba035b98c725043b041e
Simone Gentili <sensorario@gmail.com>
2018-01-05 00:32:46 +0100

    complete coverage of FilterObject

---
+++ tests/Mado/QueryBundle/Queries/Objects/FilterObjectTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Mado\QueryBundle\Tests\Queries;
+
+use Mado\QueryBundle\Queries\Objects\FilterObject;
+use PHPUnit\Framework\TestCase;
+use Mado\QueryBundle\Dictionary;
+
+class FilterObjectTest extends TestCase
+{
+    public function testDetectOperator()
+    {
+        $filter = FilterObject::fromRawFilter('foo|bar');
+        $operator = $filter->getOperator();
+        $this->assertEquals('bar', $operator);
+    }
+
+    public function testWheneverOperatorIsntDefinedUseDefaultOperator()
+    {
+        $filter = FilterObject::fromRawFilter('foo');
+        $operator = $filter->getOperator();
+        $this->assertEquals('eq', $operator);
+    }
+}
