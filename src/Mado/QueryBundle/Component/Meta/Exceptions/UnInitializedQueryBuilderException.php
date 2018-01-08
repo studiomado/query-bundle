e9e3f356740265b43992a0d037334ff4788cd72e
Alessandro Minoccheri <alessandro@studiomado.it>
2018-01-08 11:35:01 +0100

    fixed message inside exception

--- src/Mado/QueryBundle/Component/Meta/Exceptions/UnInitializedQueryBuilderException.php
+++ src/Mado/QueryBundle/Component/Meta/Exceptions/UnInitializedQueryBuilderException.php
@@ -6,11 +6,7 @@ use Throwable;
 /**
  * @since Class available since Release 2.2.0
  */
-class UnInitializedQueryBuilderException extends \Exception {
-    protected $customMessage = "Oops! Query builder was never initialized! call ::createQueryBuilder('entityName', 'alias') to start.";
-
-    public function __construct()
-    {
-        parent::__construct($this->customMessage);
-    }
+class UnInitializedQueryBuilderException extends \Exception
+{
+    protected $message = "Oops! Query builder was never initialized! call ::createQueryBuilder('entityName', 'alias') to start.";
 }
