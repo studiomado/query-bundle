fabc7097f7dd557fca09c19d0bab64504e2e19ef
Simone Gentili <sensorario@gmail.com>
2018-01-23 17:19:13 +0100

    improve coverage of QueryBuilderFactory

---
+++ tests/Mado/QueryBundle/Objects/QueryBuilderOptionsTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Mado\QueryBundle\Tests\Objects;
+
+use Mado\QueryBundle\Queries\QueryBuilderOptions;
+use PHPUnit\Framework\TestCase;
+
+class QueryBuilderOptionsTest extends TestCase
+{
+    public function testInitialize()
+    {
+        $op = QueryBuilderOptions::fromArray([
+            'filters' => 'andFilterValue',
+            'orFilters' => 'orFiltersValue',
+            'sorting' => 'asc',
+            'rel' => 'azione',
+            'printing' => 'pppp',
+            'select' => 'a,b,c',
+        ]);
+
+        $this->assertEquals('andFilterValue', $op->getAndFilters());
+        $this->assertEquals('orFiltersValue', $op->getOrFilters());
+        $this->assertEquals('asc', $op->getSorting());
+        $this->assertEquals('azione', $op->getRel());
+        $this->assertEquals('pppp', $op->getPrinting());
+        $this->assertEquals('a,b,c', $op->getSelect());
+
+        $this->assertEquals(
+            $op->get('filters'),
+            $op->getAndFilters()
+        );
+
+        $this->assertEquals(
+            null,
+            $op->get('fake')
+        );
+    }
+}
