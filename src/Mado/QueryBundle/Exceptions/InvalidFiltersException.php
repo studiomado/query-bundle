e58c042296c593c0de71d8dcf0d050ed590e7733
Saverio Cicora <saverio.cicora@studiomado.it>
2018-02-07 16:31:44 +0100

    Remove some unnecessary code

--- src/Mado/QueryBundle/Exceptions/InvalidFiltersException.php
+++ src/Mado/QueryBundle/Exceptions/InvalidFiltersException.php
@@ -3,12 +3,7 @@
 namespace Mado\QueryBundle\Exceptions;
 
 use Exception;
-use Throwable;
 
 final class InvalidFiltersException extends Exception
 {
-    public function __construct(string $message = "", int $code = 0, Throwable $previous = null)
-    {
-        parent::__construct($message, $code, $previous);
-    }
 }
