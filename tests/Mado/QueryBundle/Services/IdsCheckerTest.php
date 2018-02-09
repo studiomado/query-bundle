a549ab91e9de1c71aa7e949e6feadfcab8a1e205
Simone Gentili <sensorario@gmail.com>
2018-02-09 10:33:39 +0100

    savepoint

---
+++ tests/Mado/QueryBundle/Services/IdsCheckerTest.php
@@ -0,0 +1,110 @@
+<?php
+
+use Mado\QueryBundle\Objects\Filter;
+use Mado\QueryBundle\Services\IdsChecker;
+use PHPUnit\Framework\TestCase;
+
+final class IdsCheckerTest extends TestCase
+{
+    /**
+     * @expectedException \Mado\QueryBundle\Exceptions\ForbiddenContentException
+     */
+    public function testQueryStringCantContainsInvalidIds()
+    {
+        $querystringIds = '1';
+        $additionalFiltersIds = '2,3';
+
+        $filter = Filter::box([
+            'ids'  => ['list' => []],
+            'path' => '_embedded.foo.bar',
+        ]);
+
+        $cheker = new IdsChecker();
+        $cheker->setObjectFilter($filter);
+        $cheker->setAdditionalFiltersIds($additionalFiltersIds);
+        $cheker->setFiltering(['filtering' => $querystringIds]);
+
+        $cheker->validateIds();
+    }
+
+    public function testValidIdsOverwriteFilteringWithIntersection()
+    {
+        $querystringIds = '2';
+        $additionalFiltersIds = '2,3';
+
+        $filter = Filter::box([
+            'ids'  => ['list' => []],
+            'path' => '_embedded.foo.bar',
+        ]);
+
+        $cheker = new IdsChecker();
+        $cheker->setObjectFilter($filter);
+        $cheker->setAdditionalFiltersIds($additionalFiltersIds);
+        $cheker->setFiltering(['filtering' => $querystringIds]);
+
+        $cheker->validateIds();
+
+        $this->assertEquals('2', $cheker->getFinalFilterIds());
+    }
+
+    public function testAdditionalFilterInBlacList()
+    {
+        $querystringIds = '2';
+        $additionalFiltersIds = '66';
+
+        $additionalFilterObject = Filter::box([
+            'ids'  => ['nlist' => []],
+            'path' => '_embedded.foo.bar',
+        ]);
+
+        $cheker = new IdsChecker();
+        $cheker->setObjectFilter($additionalFilterObject);
+        $cheker->setAdditionalFiltersIds($additionalFiltersIds);
+        $cheker->setFiltering(['field' => $querystringIds]);
+
+        $cheker->validateIds();
+
+        $this->assertEquals('2', $cheker->getFinalFilterIds());
+    }
+
+    public function testProvideListOfRightIds()
+    {
+        $querystringIds = '2';
+        $additionalFiltersIds = '66';
+
+        $additionalFilterObject = Filter::box([
+            'ids'  => ['nlist' => []],
+            'path' => '_embedded.foo.bar',
+        ]);
+
+        $cheker = new IdsChecker();
+        $cheker->setObjectFilter($additionalFilterObject);
+        $cheker->setAdditionalFiltersIds($additionalFiltersIds);
+        $cheker->setFiltering(['field' => $querystringIds]);
+
+        $cheker->validateIds();
+
+        $this->assertEquals('_embedded.foo.bar.id|list', $cheker->getFilterKey());
+    }
+
+    public function testOverwriteFilterKeyIfNecessary()
+    {
+        $querystringIds = '2';
+        $additionalFiltersIds = '66';
+
+        $additionalFilterObject = Filter::box([
+            'ids'  => ['nlist' => []],
+            'path' => '_embedded.foo.bar',
+        ]);
+
+        $cheker = new IdsChecker();
+        $cheker->setObjectFilter($additionalFilterObject);
+        $cheker->setFilterKey('foo');
+        $cheker->setAdditionalFiltersIds($additionalFiltersIds);
+        $cheker->setFiltering(['field' => $querystringIds]);
+
+        $cheker->validateIds();
+
+        $this->assertNotEquals('foo', $cheker->getFilterKey());
+    }
+}
